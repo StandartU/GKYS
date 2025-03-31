5d6ad44eda0282ed174cc007b7d6c1a440c3c167
Ownosh <144677630+Ownosh@users.noreply.github.com>
2025-03-31 10:31:40 +0300

    Update build.gradle.kts

--- WalkiePaws/app/build.gradle.kts
+++ WalkiePaws/app/build.gradle.kts
@@ -60,5 +60,7 @@ dependencies {
     implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation ("androidx.core:core-ktx:1.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
 }
