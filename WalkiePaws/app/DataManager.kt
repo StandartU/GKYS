031022ab60a42b06eba5de5f2f1a8d1278d86d7d
Ownosh <144677630+Ownosh@users.noreply.github.com>
2025-04-10 00:40:26 +0300

    Delete DataManager.kt

--- WalkiePaws/app/DataManager.kt
+++
@@ -1,9 +0,0 @@
-object DataManager {
-    var currentCharacterIndex = 0
-    val characters = listOf(
-        R.drawable.gepard,
-        R.drawable.kangaroo,
-        R.drawable.hamster,
-        R.drawable.rabbit
-    )
-}
